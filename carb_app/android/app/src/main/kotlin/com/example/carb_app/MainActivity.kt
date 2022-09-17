e3ba12e8578f8188993a77cf66fb5c2c1b6b54eb
jasonyuan12345 <113218775+jasonyuan12345@users.noreply.github.com>
2022-09-17 11:53:53 -0700

    Added first screen

---
+++ carb_app/android/app/src/main/kotlin/com/example/carb_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ carb_app/lib/homescreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("wadwad"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 25,
+              child: Image(
+                  image: NetworkImage("https://media.istockphoto.com/vectors/simple-apple-in-flat-style-vector-illustration-vector-id1141529240?k=20&m=1141529240&s=612x612&w=0&h=_j9z-sPT6AqFDSSXHnSYWrXOvDOgyMmSdTUrBiZULCo=")
+              ),
+            ),
+            Expanded(
+              flex: 75,
+              child: Column(
+                children: [
+                  Text(" Title "),
+                  ElevatedButton(onPressed: () {}, child: Text("Title"),)
+                ],
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
